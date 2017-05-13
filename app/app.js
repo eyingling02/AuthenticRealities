12760d47b565da905f42e134bee32768ba0cd2c3
Roger <roger.e.clayton@gmail.com>
2017-05-13 15:01:05 -0700

    appending an element on a timeout working

--- app/app.js
+++ app/app.js
@@ -1,3 +1,24 @@
 $( document ).ready(function() {
     console.log("Still connected");
+    test();
+    // showShoe();
+    setTimeout(function(){ appendShoe(); }, 8000);
 });
+
+var test = function() {
+  console.log("called");
+}
+
+var appendShoe = function() {
+  var entity = $("#theentity");
+  var shoeModel = "<a-collada-model position='0 0.05 -2.5' scale='.01 .01 .01' src='#shoe' ></a-collada-model>";
+  entity.append(shoeModel);
+
+}
+
+var showShoe = function() {
+  var shoeModel = $("shoe-model")
+  debugger
+  shoeModel.setAttribute("visible", true);
+
+}
