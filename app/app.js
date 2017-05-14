b80d1a99920cc68edb49be2bc2a105d26bcdb42a
eyingling02 <eyingling02@gmail.com>
2017-05-13 18:01:39 -0700

    Add working counter

--- app/app.js
+++ app/app.js
@@ -1,24 +1,20 @@
 $( document ).ready(function() {
-    console.log("Still connected");
-    test();
-    // showShoe();
-    setTimeout(function(){ appendShoe(); }, 8000);
+    yearIncrease();
 });
 
-var test = function() {
-  console.log("called");
+var yearIncrease = function() {
+  var counter = 0;
+  var color = document.getElementById('color');
+  setInterval(function() {
+    if (counter < 35) {
+    color.setAttribute("text", "color: red; align: center; width: 6; value: "+ counter);
+    counter++;
+    }
+  }, 1000);
 }
 
 var appendShoe = function() {
   var entity = $("#theentity");
   var shoeModel = "<a-collada-model position='0 0.05 -2.5' scale='.01 .01 .01' src='#shoe' ></a-collada-model>";
   entity.append(shoeModel);
-
-}
-
-var showShoe = function() {
-  var shoeModel = $("shoe-model")
-  debugger
-  shoeModel.setAttribute("visible", true);
-
 }
