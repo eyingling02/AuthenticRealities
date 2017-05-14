b7b4ed68958bfbe22cce216e9e33ce50d343e3a6
Roger <roger.e.clayton@gmail.com>
2017-05-14 12:18:19 -0700

    adjusts position for elements and adds in moneybags resources

--- app/app.js
+++ app/app.js
@@ -1,11 +1,11 @@
 $( document ).ready(function() {
     // appendHouse();
-    yearIncrease(70000, 5, 4735208, 31);
+    yearIncrease(100000, 7, 8890278, 26, "#whitemale");
 
 });
 
 
-var yearIncrease = function(baseSalary, canBuyHouse, accumulatedWealthNumber, canRetire) {
+var yearIncrease = function(baseSalary, canBuyHouse, accumulatedWealthNumber, canRetire, demographic) {
   var counter = 0;
   var year = document.getElementById('timekeeper');
   var salary = document.getElementById('salary');
@@ -19,9 +19,10 @@ var yearIncrease = function(baseSalary, canBuyHouse, accumulatedWealthNumber, ca
       }
       if (counter === 30){
         appendAccumulatedWealth(accumulatedWealthNumber);
+        displayEarnings(demographic);
       };
     year.setAttribute("text", "color: black; align: center; width: 6; value: Year: "+ counter);
-    salary.setAttribute("text", "color: blue; align: center; width: 6; value: Salary: "+ baseSalary);
+    salary.setAttribute("text", "color: blue; align: center; width: 6; value: Salary: $"+ baseSalary);
     counter++;
     baseSalary = Math.floor(baseSalary * 1.05);
 
@@ -41,18 +42,24 @@ var yearIncrease = function(baseSalary, canBuyHouse, accumulatedWealthNumber, ca
 var appendHouse = function() {
   console.log("isthis on?")
   var house = $("#house-img");
-  house.replaceWith("<a-box src='#home' position='-2 0 -4' 'depth=0' 'height=4' 'width=0.5' rotation='-90 180 180'></a-box>")
-   
+  house.replaceWith("<a-box src='#home' position='-2 0 -5.3' 'depth=0' 'height=4' 'width=0.5' rotation='-90 180 180'></a-box>")
+
 }
 
 var appendAccumulatedWealth = function(accumulatedWealthNumber) {
 
   var retirementMoney = $("#accumulated-wealth");
-  retirementMoney.replaceWith("<a-entity id='accumulated-wealth' position='0 1 0' scale='2 2 2' rotation='-90 180 180' text='color: purple; align: center; value:" + accumulatedWealthNumber + "; width: 1.5'></a-entity>")
+  retirementMoney.replaceWith("<a-entity id='accumulated-wealth' position='2 0 -5' scale='5 5 5' rotation='-90 180 180' text='color: green; align: center; value: Earnings: $" + accumulatedWealthNumber + "; width: 1.5'></a-entity>")
 }
 
 var appendRetirementBadge = function() {
-  
+
   var retirementBadge = $("#retirement-year");
-  retirementBadge.replaceWith("<a-circle src='#retirement' position='0 0 -4' radius='.75' rotation='-90 180 180'></a-circle>")
+  retirementBadge.replaceWith("<a-circle src='#retirement' position='-2.5 0 -3.7' radius='.70' rotation='-90 180 180'></a-circle>")
+}
+
+var displayEarnings = function(demographic) {
+
+  var moneyBags = $("#demographic");
+  moneyBags.replaceWith("<a-image src=" + demographic + " position='-3.5 0 -2.7' rotation='-90 180 180'></a-image>")
 }
