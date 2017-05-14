083f6e94e8a6d0a0ad41bc4e96af162c29b76073
Merty McGraw <mertymcgraw@gmail.com>
2017-05-13 21:03:09 -0700

    Add salary counter, add house when counter reaches canBuyHouse argument value

--- app/app.js
+++ app/app.js
@@ -1,21 +1,54 @@
 $( document ).ready(function() {
-    yearIncrease();
-    appendShoe();
+    // appendHouse();
+    yearIncrease(100000, 12);
+    
 });
 
-var yearIncrease = function() {
+
+var yearIncrease = function(baseSalary, canBuyHouse) {
   var counter = 0;
-  var color = document.getElementById('timekeeper');
+  var year = document.getElementById('timekeeper');
+  var salary = document.getElementById('salary');
   setInterval(function() {
     if (counter < 31) {
-    color.setAttribute("text", "color: red; align: center; width: 6; value: "+ counter);
+      if (counter === canBuyHouse){
+        appendHouse();
+      }
+      // if (counter === 30){
+      //   accumulatedWealth
+      // };
+    year.setAttribute("text", "color: red; align: center; width: 6; value: Year "+ counter);
+    salary.setAttribute("text", "color: red; align: center; width: 6; value: Salary "+ baseSalary);
     counter++;
+    baseSalary = Math.floor(baseSalary * 1.05);
+
     }
   }, 1000);
 }
 
-var appendShoe = function() {
-  var entity = $("#assets");
-  var shoeModel = "<a-collada-model position='0 0.05 -2.5' scale='.01 .01 .01' src='#shoe' ></a-collada-model>";
-  entity.append(shoeModel);
+var appendHouse = function() {
+  var entity = document.querySelector("a-entity");
+  var houseModel =  document.createElement('a-collada-model');
+  houseModel.setAttribute("position", '0 0.1 -3.5');
+  houseModel.setAttribute("scale", '.005 .005 .005');
+  houseModel.setAttribute("src", '#house');
+  entity.appendChild(houseModel);
 }
+
+// var appendAccumulatedWealth = function() {
+//   var entity = document.querySelector("a-entity");
+//   var retirementMoney =  document.createElement('a-collada-model');
+//   houseModel.setAttribute("position", '0 0.1 -3.5');
+//   houseModel.setAttribute("scale", '.005 .005 .005');
+//   houseModel.setAttribute("src", '#house');
+//   entity.appendChild(houseModel);
+// }
+
+
+ // var scene = document.querySelector('a-scene');
+ //    var cylinder = document.createElement('a-cylinder');
+ //    cylinder.setAttribute('color', '#FF9500');
+ //    cylinder.setAttribute('height', '2');
+ //    cylinder.setAttribute('radius', '0.75');
+ //    cylinder.setAttribute('position', '3 1 0');
+ //    scene.appendChild(cylinder);
