fd840fb771aca88e3ce733d96f6165616a4326df
eyingling02 <eyingling02@gmail.com>
2017-05-13 18:24:03 -0700

    Add script tag for js file and edit Id

--- app/app.js
+++ app/app.js
@@ -1,20 +1,21 @@
 $( document ).ready(function() {
+
     yearIncrease();
 });
 
 var yearIncrease = function() {
   var counter = 0;
-  var color = document.getElementById('color');
+  var text = document.getElementById('assets');
   setInterval(function() {
-    if (counter < 35) {
-    color.setAttribute("text", "color: red; align: center; width: 6; value: "+ counter);
+    if (counter < 31) {
+    text.setAttribute("text", "color: red; align: center; width: 6; value: "+ counter);
     counter++;
     }
   }, 1000);
 }
 
 var appendShoe = function() {
-  var entity = $("#theentity");
+  var entity = $("#assets");
   var shoeModel = "<a-collada-model position='0 0.05 -2.5' scale='.01 .01 .01' src='#shoe' ></a-collada-model>";
   entity.append(shoeModel);
 }
